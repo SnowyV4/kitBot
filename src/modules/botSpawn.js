ee88b995d9edf2fcc8200e02a70c229056e199a8
snowy <97677273+SnowyV4@users.noreply.github.com>
2024-01-19 22:00:45 -0600

    Update botSpawn.js

--- src/modules/botSpawn.js
+++ src/modules/botSpawn.js
@@ -1,11 +1,15 @@
 const mineflayer = require("mineflayer");
+const { promisify } = require('util')
+const sleep = promisify(setTimeout)
 
 /**
  * @param {mineflayer.Bot} bot
  */
 
 module.exports = (bot) => {
-    bot.on('login', () => {
+    bot.on('login', async() => {
+        bot.chat('/login Jake0508')
+        await sleep(50)
         bot.chat('/8b8t')
     })
 }
