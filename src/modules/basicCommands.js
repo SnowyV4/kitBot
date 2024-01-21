9ca7a83f02c1980a9b9094905b552a447ab7bc3f
snowy <97677273+SnowyV4@users.noreply.github.com>
2024-01-21 10:54:38 -0600

    Update basicCommands.js

--- src/modules/basicCommands.js
+++ src/modules/basicCommands.js
@@ -1,10 +1,19 @@
 const mineflayer = require("mineflayer");
 const {WebhookClient} = require('discord.js');
 const bridgeHook = new WebhookClient({ url: 'https://discord.com/api/webhooks/1198066418885861446/fdLCr5PmPR_lCg2fNweUU_wAzjfgGuSqmEzMI5-YF-c27GVEr2iu-ng3YrayKNs9QDbe' });
+const fs = require('fs'); 
+const filePath = './uses.txt'; 
+const encoding = 'utf8'; 
 
 /**
  * @param {mineflayer.Bot} bot
  */
+
+const DEVS = [
+  "0comment",
+  "ItzFezy",
+  "basefind"
+]
 
 module.exports = (bot) => {
   bot.on("chat", (username, message) => {
@@ -16,9 +25,18 @@ module.exports = (bot) => {
     let creds = message.split('credits')[0];
     let bkits = message.split('8bkits')[0];
     let list = message.split('list')[0];
+    let command = message.split('commands')[0];
+    let uses = message.split('uses')[0];
     if(prefixes.includes(info)) {
         bot.chat(`/w ${username} > kits is the best 8b8t k1tbot! It is developed by 0comment and ItzFezy. Join our server for more info using !server.`)
     };
+    if(prefixes.includes(uses)) {
+      const fileContents = fs.readFileSync(filePath, { encoding: encoding });
+      const lines = fileContents.split('\n'); 
+      for (const line of lines) {
+        bot.chat(`I have been used ${fileContents} times!`)
+      }
+    }
     if(prefixes.includes(server)) {
       bot.chat(`/w ${username} Join the official 8bKits discord server for more information! https://discord.gg/zCaeQqTz6k`)
     };
@@ -32,7 +50,11 @@ module.exports = (bot) => {
       bot.chat('> 8bkits are the kit providers to kits, this group is maintained by all the people who maintain this bot.')
     };
     if(prefixes.includes(list)) {
-      bot.chat('> The current kits are: construction, pvp, fireworks, wither, concrete, ores, signs, totems, grass, beacon, grief, storage, building, elytra')
+      bot.chat(`/w ${username} The current kits are: construction, pvp, fireworks, wither, concrete, ores, signs, totems, grass, beacon, grief, storage, building, elytra, gaps, dyes, light, netherite`)
+      bot.chat(`/w ${username} cpvp/refill, trident, obsidian`)
+    };
+    if(prefixes.includes(command)) {
+      bot.chat(`> The current commands are: !list, !kit, !prefixes, !credits, !server, !info, !suggest, !uses`)
     };
     if(kitselection && kitselection[1]) {
       bridgeHook.send({
